f8785bf3b9cbff640e0cc18f84802018e8f7d8f1
Samiya Fatima <samiyafatima82@gmail.com>
2025-03-23 03:51:50 -0400

    Implemented custom error page from lesson 6

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,16 +16,16 @@ export function dashboardLoader(){
 }
 
 //action
-export async function dashboardAction({request}) {
+export async function dashboardAction({ request }) {
     const data = await request.formData();
-    const formData = Object.fromEntries(data)
+    const formData = Object.fromEntries(data);
     try {
-        throw new Error("Done")
+      localStorage.setItem("userName", JSON.stringify(formData.userName))
+      return toast.success(`Welcome, ${formData.userName}`)
     } catch (e) {
-        throw new Error("There was a problem creating your account.")
+      throw new Error("There was a problem creating your account.")
     }
-
-}
+  }  
 
 const Dashboard = () => {
     const { userName } = useLoaderData()
