9e9f8e6b55cf6f7c1c79f39a1126f97fd1b6ad9a
Samiya Fatima <samiyafatima82@gmail.com>
2025-03-23 03:26:22 -0400

    Added login form from lesson 5

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,20 +3,39 @@ import { useLoaderData } from "react-router-dom";
 //helper functions
 import { fetchData } from "../helpers"
 
+//components
+import Intro from "../components/Intro";
+
+//library imports
+import { toast } from "react-toastify";
+
 //loader
 export function dashboardLoader(){
     const userName = fetchData("userName");
     return { userName }
 }
 
+//action
+export async function dashboardAction({request}) {
+    const data = await request.formData();
+    const formData = Object.fromEntries(data)
+    try {
+        throw new Error("Done")
+    } catch (e) {
+        throw new Error("There was a problem creating your account.")
+    }
+
+}
+
 const Dashboard = () => {
     const { userName } = useLoaderData()
 
     return(
-        <div>
-           <h1>{userName}</h1> 
-        Dashboard
-        </div>
+        <>
+           {userName ? (<p>{userName}</p>) : 
+           <Intro />}
+        
+        </>
     )
 }
 export default Dashboard
